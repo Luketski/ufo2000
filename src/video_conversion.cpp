907ff270d3481c38c9491c82afb926e0f03af05f
Luketski <n5g@freemail.hu>
2011-07-27 08:14:24 +0000

    Added the GPL copyright notices, and added links to the original sources of the code copied from elsewhere.

--- src/video_conversion.cpp
+++ src/video_conversion.cpp
@@ -1,3 +1,23 @@
+/*
+This file is part of UFO2000 (http://ufo2000.sourceforge.net)
+
+Copyright (C) 2000-2001  Alexander Ivanov aka Sanami
+Copyright (C) 2002       ufo2000 development team
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 
 
@@ -14,6 +34,7 @@
 
 
 
+// these 2 functions were copied from here:  http://forum.doom9.org/archive/index.php/t-109311.html
 
 /*================================== RGB-YUV =================================*/
 void rgb_to_yuv (float r[8][8], float g[8][8], float b[8][8], float y[8][8], float u[8][8], float v[8][8])
